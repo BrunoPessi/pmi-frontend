6923f24a44e8d6b023cff3ef32948a22df87507a
arthur <arthur@margarete.margarete>
2020-03-13 15:36:09 -0300

    adicionado botoes, axios,etc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './IndexPage';
+import App from './pages/IndexPg';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
